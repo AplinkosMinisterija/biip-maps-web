73f0f241d06f5e714ff08d6b04327560471b1aa4
Pranciskus Ambrazas <p.ambrazas@gmail.com>
2023-11-22 14:10:51 +0200

    converting polygons, linestrings to points

--- src/routes/edit.vue
+++ src/routes/edit.vue
@@ -153,7 +153,12 @@ const selectSearch = (match: any) => {
   }
   if (!match?.geom) return;
 
-  mapDraw.value.setFeatures(match.geom, !!query.multi).edit();
+  mapDraw.value
+    .setFeatures(match.geom, {
+      append: !!query.multi,
+      types: drawTypes.value.map((i) => i.type),
+    })
+    .edit();
 };
 
 const toggleDrawType = (type: string) => {
