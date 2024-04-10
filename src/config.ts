44aafc968dec27af9d2587fdb9b3fe4d064aca4d
Pranciskus Ambrazas <p.ambrazas@gmail.com>
2024-04-10 08:05:18 +0300

    update proxy urls

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,13 @@
 export const qgisServerHost = import.meta.env.VUE_APP_QGIS_SERVER || 'https://gis.biip.lt';
 
-export const uetkApiHost = '/proxy/uetk';
-export const rusysApiHost = '/proxy/rusys';
-export const gyvunaiApiHost = '/proxy/gyvunai';
-export const zvejybaApiHost = '/proxy/zvejyba';
-export const zuvinimasApiHost = '/proxy/zuvinimas';
-export const medziokleApiHost = '/proxy/medziokle';
+const proxyUrl = import.meta.env.VUE_APP_PROXY_BASE_URL ?? '/proxy';
+
+export const uetkApiHost = `${proxyUrl}/uetk`;
+export const rusysApiHost = `${proxyUrl}/rusys`;
+export const gyvunaiApiHost = `${proxyUrl}/gyvunai`;
+export const zvejybaApiHost = `${proxyUrl}/zvejyba`;
+export const zuvinimasApiHost = `${proxyUrl}/zuvinimas`;
+export const medziokleApiHost = `${proxyUrl}/medziokle`;
 export const qgisServerUrl = `${qgisServerHost}/qgisserver`;
 export const qgisApiUrl = `${qgisServerHost}/api`;
 export const qgisTilesUrl = `${qgisServerHost}/tiles`;
