f4cef3b6f5e5d776b4b0bf5a3482ee28f25ac357
Pranciskus Ambrazas <p.ambrazas@gmail.com>
2024-04-18 13:06:39 +0300

    changing medziokle draw map projection

--- src/routes/medziokle/draw.vue
+++ src/routes/medziokle/draw.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <UiMap :show-scale-line="true" :show-search="true" @search="filtersStore.search = $event">
+    <UiMap
+      :show-scale-line="true"
+      :projection="projection3857"
+      :show-search="true"
+      @search="filtersStore.search = $event"
+    >
       <template v-if="filtersStore.active" #filtersContent>
         <Search
           v-if="filtersStore.isActive('search')"
@@ -13,51 +18,58 @@
   </div>
 </template>
 <script setup lang="ts">
-import { inject, computed } from 'vue';
-import { useRoute } from 'vue-router';
-import { useFiltersStore } from '@/stores/filters';
-import Search from '@/components/search/Index.vue';
+import { inject, computed } from "vue";
+import { useRoute } from "vue-router";
+import { useFiltersStore } from "@/stores/filters";
+import Search from "@/components/search/Index.vue";
 import {
-  geoportalTopo,
   geoportalOrto,
-  geoportalTopoGray,
   parseRouteParams,
   markerLayer,
   huntingService,
-} from '@/utils';
+  projection3857,
+  projection,
+  convertFeatureCollectionProjection,
+  vectorPositron,
+  vectorBright,
+} from "@/utils";
 
 const filtersStore = useFiltersStore();
-const postMessage: any = inject('postMessage');
-const mapLayers: any = inject('mapLayers');
+const postMessage: any = inject("postMessage");
+const mapLayers: any = inject("mapLayers");
 const $route = useRoute();
-const events: any = inject('events');
+const events: any = inject("events");
 const mapDraw = computed(() => mapLayers.getDraw(markerLayer.id).enableContinuousDraw());
 
-const query = parseRouteParams($route.query, ['preview', 'mpvId']);
+const query = parseRouteParams($route.query, ["preview", "mpvId"]);
 const huntingServiceFilters = mapLayers.filters(huntingService.id);
 
-events.on('geom', (data: any) => {
-  mapDraw.value.setFeatures(data.geom || data);
+events.on("geom", (data: any) => {
+  mapDraw.value.setFeatures(data.geom || data, { dataProjection: projection });
 });
 
 mapLayers
-  .addBaseLayer(geoportalTopoGray.id)
-  .addBaseLayer(geoportalTopo.id)
+  .addBaseLayer(vectorPositron.id)
+  .addBaseLayer(vectorBright.id)
   .addBaseLayer(geoportalOrto.id)
   .add(huntingService.id);
 
 if (query.mpvId) {
-  huntingServiceFilters.on('mpv_info_geom').set('mpv_id', query.mpvId);
+  huntingServiceFilters.on("mpv_info_geom").set("mpv_id", query.mpvId);
   await mapLayers.zoom(huntingService.id, { addStroke: false });
 }
 
 if (!query.preview) {
-  mapDraw.value.start('Point').on(['change', 'remove'], ({ features }: any) => {
-    if (typeof features === 'string') {
+  mapDraw.value.start("Point").on(["change", "remove"], ({ features }: any) => {
+    if (features) {
+      features = convertFeatureCollectionProjection(features, projection3857, projection);
+    }
+
+    if (typeof features === "string") {
       const geom = JSON.parse(features);
       mapLayers.zoomToFeatureCollection(geom);
     }
-    postMessage('data', features);
+    postMessage("data", features);
   });
 }
 </script>
