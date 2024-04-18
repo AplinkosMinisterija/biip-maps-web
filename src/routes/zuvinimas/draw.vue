028b20c4f9562fd397247c5dba56a183b98edf3c
Pranciskus Ambrazas <p.ambrazas@gmail.com>
2024-04-18 12:42:31 +0300

    apply 3857 projection to zuvinimas draw

--- src/routes/zuvinimas/draw.vue
+++ src/routes/zuvinimas/draw.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <UiMap :show-scale-line="true" :show-search="true" @search="filtersStore.search = $event">
+    <UiMap
+      :show-scale-line="true"
+      :projection="projection3857"
+      :show-search="true"
+      @search="filtersStore.search = $event"
+    >
       <template v-if="filtersStore.active" #filtersContent>
         <Search
           v-if="filtersStore.isActive('search')"
@@ -13,51 +18,54 @@
   </div>
 </template>
 <script setup lang="ts">
-import { inject, computed } from 'vue';
-import { useRoute } from 'vue-router';
-import { useFiltersStore } from '@/stores/filters';
+import { inject, computed } from "vue";
+import { useRoute } from "vue-router";
+import { useFiltersStore } from "@/stores/filters";
 import {
-  geoportalTopo,
   geoportalOrto,
-  geoportalTopoGray,
+  projection3857,
   zuvinimasService,
   uetkService,
   parseRouteParams,
   markerLayer,
-} from '@/utils';
+  convertFeatureCollectionProjection,
+  vectorBright,
+  vectorPositron,
+  projection,
+} from "@/utils";
 
 const filtersStore = useFiltersStore();
-const postMessage: any = inject('postMessage');
-const mapLayers: any = inject('mapLayers');
+const postMessage: any = inject("postMessage");
+const mapLayers: any = inject("mapLayers");
 const $route = useRoute();
-const events: any = inject('events');
+const events: any = inject("events");
 const mapDraw = computed(() => mapLayers.getDraw(markerLayer.id).enableContinuousDraw());
 
 const query = parseRouteParams($route.query, [
-  'id',
-  'createdBy',
-  'tenantId',
-  'userId',
-  'stockingCustomer',
-  'preview',
+  "id",
+  "createdBy",
+  "tenantId",
+  "userId",
+  "stockingCustomer",
+  "preview",
 ]);
 
 const zuvinimasServiceFilters = mapLayers.filters(zuvinimasService.id);
-const filters = computed(() => zuvinimasServiceFilters.on('fish_stockings'));
+const filters = computed(() => zuvinimasServiceFilters.on("fish_stockings"));
 
-events.on('geom', (data: any) => {
-  mapDraw.value.setFeatures(data.geom || data);
+events.on("geom", (data: any) => {
+  mapDraw.value.setFeatures(data.geom || data, { dataProjection: projection });
 });
 
 if (query.tenantId) {
-  filters.value.set('$or', [
+  filters.value.set("$or", [
     { tenant_id: query.tenantId },
     { stocking_customer_id: query.tenantId },
   ]);
 }
 
 if (query.userId) {
-  filters.value.set('created_by', query.userId).set('tenant_id', { $exists: false });
+  filters.value.set("created_by", query.userId).set("tenant_id", { $exists: false });
 }
 
 const selectSearch = (match: any) => {
@@ -69,17 +77,20 @@ const selectSearch = (match: any) => {
 };
 
 mapLayers
-  .addBaseLayer(geoportalTopoGray.id)
-  .addBaseLayer(geoportalTopo.id)
+  .addBaseLayer(vectorPositron.id)
+  .addBaseLayer(vectorBright.id)
   .addBaseLayer(geoportalOrto.id)
   .add(uetkService.id)
   .enableLocationTracking();
 
-mapDraw.value.setIcon('pin-water', { align: 'top', size: 4 });
+mapDraw.value.setIcon("pin-water", { align: "top", size: 4 });
 
 if (!query.preview) {
-  mapDraw.value.start('Point').on(['change', 'remove'], ({ features }: any) => {
-    postMessage('userObjects', features);
+  mapDraw.value.start("Point").on(["change", "remove"], ({ features }: any) => {
+    if (features) {
+      features = convertFeatureCollectionProjection(features, projection3857, projection);
+    }
+    postMessage("userObjects", features);
   });
 }
 </script>
