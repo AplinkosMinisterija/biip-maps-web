8175fd9576e5b2f48ef242e1583b0cab62c8fc22
LWangllix <vanglikas.lukas@gmail.com>
2024-07-01 14:47:53 +0300

    update

--- src/components/ui/sidebar/Index.vue
+++ src/components/ui/sidebar/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     v-if="isOpen"
-    class="absolute right-0 top-0 h-screen overflow-y-scroll p-4 sm:w-96 max-w-screen-sm z-40 shadow-md bg-white"
+    class="absolute right-0 top-0 h-screen overflow-y-scroll p-4 w-full sm:w-96 max-w-screen-sm z-40 shadow-md bg-white"
   >
     <div class="flex items-center justify-between gap-3 w-full mb-3">
       <span class="font-semibold">{{ title }}</span>
