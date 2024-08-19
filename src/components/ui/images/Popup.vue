236435be7caa488c606341005af402775d1a67d1
Pranciskus Ambrazas <p.ambrazas@gmail.com>
2024-08-19 09:19:51 +0300

    fix

--- src/components/ui/images/Popup.vue
+++ src/components/ui/images/Popup.vue
@@ -8,22 +8,24 @@
         class="fixed top-0 left-0 h-screen w-screen flex lg:items-center items-end justify-center z-50 bg-black bg-opacity-70"
         @click="close"
       />
-
-      <div class="relative z-50 flex items-center select-none">
-        <UiIcon
-          name="chevron-left"
-          class="text-white cursor-pointer"
-          :size="40"
-          @click="setIndex(-1)"
-        />
-        <img :src="images[currentImageIndex]" class="max-h-screen p-16" />
-
-        <UiIcon
-          name="chevron-right"
-          class="text-white cursor-pointer"
-          :size="40"
-          @click="setIndex(1)"
-        />
+      <div class="relative z-50 h-screen p-2 md:p-8 xl:p-16 w-full">
+        <div class="h-full w-full flex items-center justify-center select-none">
+          <UiIcon
+            name="chevron-left"
+            class="text-white cursor-pointer shrink-0"
+            :size="40"
+            @click="setIndex(-1)"
+          />
+          <div class="m-2 md:m-8 h-full flex items-center">
+            <img :src="images[currentImageIndex]" class="max-h-full max-w-full" />
+          </div>
+          <UiIcon
+            name="chevron-right"
+            class="text-white cursor-pointer shrink-0"
+            :size="40"
+            @click="setIndex(1)"
+          />
+        </div>
       </div>
     </div>
   </div>
